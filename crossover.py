2b67c572357edc30dfeb0f7332cd7fe255f30aca
Nuno Lourenço <pikinhass@gmail.com>
2010-05-10 18:39:16 +0100

    Cut and paste xover implemented and ready for integration with the main program

--- crossover.py
+++ crossover.py
@@ -1,29 +1,71 @@
+from mutation import apply_rotation
+from commons import cenas
+from fitness_function import *
+
+
 def crossover(protein,parents):
-    possible_rotations = [90,180,270]
-    is_valid = True
+    possible_rotations = [0,90,180,270]
+    is_invalid = True
     while is_invalid:
+        is_invalid = False
         p1 = choice(parents)
         p2 = choice(parents)
         parent1 = p1[0]
         parent2 = p2[0]
         offspring = []
         cut_point = choice(range(len(protein)))
-        rotation_to_apply = choice(possible_rotations)        
+        cut_point = 13
+        
+        rotation_to_apply = choice(possible_rotations)
         offspring += parent1[:cut_point+1]
-        is_invalid = False
-        for i in range(cut_point + 1,len(parent1)):
-            #bring to (0,0) to rotate using cut_point as reference
-            am_to_rotate = (lambda p1,p2: (p1[0] - p2[0], p1[1] - p2[1])) (parent2[i], parent1[cut_point])
-            new_pos = apply_rotation(am_to_rotate,rotation_to_apply)
-            #put the point back in the position it belongs
-            new_pos = (lambda p1,p2: (p1[0] + p2[0], p1[1] + p2[1])) (new_pos, parent1[cut_point])
-            if new_pos in offspring:
-                is_invalid  = True
-                break
+        pivot = offspring[cut_point]
+        offspring_tail = remap_parent2_coords(pivot,parent2[cut_point:],rotation_to_apply)
+        print offspring_tail
+        if offspring_tail == None:
+            is_invalid = True
+        else:
+            for c in offspring_tail:
+                if c in offspring:
+                    is_invalid = True
+                    break
             else:
-                offspring.append(new_pos)
+                offspring += offspring_tail
     new_ind = (offspring,fitness_function(protein,offspring))
     return new_ind
+    
+def remap_parent2_coords(reference_am,pos_cut_point_parent2,angle_to_rotate):
+    remaped_parent = []
+    #gets the directons which the amino-acids are arranged
+    for i in range(len(pos_cut_point_parent2)-1):
+         remaped_parent.append((pos_cut_point_parent2[i+1][0] - pos_cut_point_parent2[i][0],pos_cut_point_parent2[i+1][1] - pos_cut_point_parent2[i][1]))
+    
+    #applys random_rotation
+    for i in range(len(remaped_parent)):
+        remaped_parent[i] = apply_rotation(remaped_parent[i],angle_to_rotate)
+    
+    #puts the new amino-acids configuraton according to the cut_point amino-acid
+    temp = [reference_am]
+    for i in range(len(remaped_parent)):
+        new_pos = (remaped_parent[i][0] + temp[i][0],remaped_parent[i][1] + temp[i][1])
+        if(new_pos in temp):
+            print "exists"
+            return None
+        else:
+            temp.append(new_pos)
+    
+    remaped_parent = temp[1:]
+
+    return remaped_parent
+
+    
+    
+        
+    
+parent1 = ([(0,0),(1,0),(1,1),(1,2),(0,2),(0,1),(-1,1),(-1,2),(-2,2),(-2,3),(-3,3),(-3,2),(-3,1),(-2,1),(-2,0),(-2,-1),(-3,-1),(-3,-2),(-3,-3),(-2,-3)],0)
+parent2 = ([(0,0),(1,0),(1,-1),(1,-2),(1,-3),(1,-4),(0,-4),(-1,-4),(-1,-3),(-1,-2),(-2,-2),(-3,-2),(-3,-3),(-4,-3),(-5,-3),(-5,-2),(-6,-2),(-6,-3),(-6,-4),(-5,-4)],0)
+p = "BWBWWBBWBWWBWBBWWBWB"
+
+print crossover(p,[parent1,parent2])
             
             
     
